9fed55618ef77317ced50a5d65e6c6020765d826
anushabollineni <41730220+anushabollineni@users.noreply.github.com>
2018-09-08 11:30:51 +0530

    Create gv82.java

---
+++ gv82.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+double a,b,c;
+a=sc.nextDouble();
+b=sc.nextDouble();
+c=a*b;
+System.out.format("%.5f",c);
+	}
+}
